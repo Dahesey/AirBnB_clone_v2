f2d2bc147b660f5496132b8984034c3d07757663
Cynthia Dodzi <dahesey@gmail.com>
2024-03-24 20:08:14 +0000

    Updated task 1 with a new route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+A script to start a simple flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+""" Define route method """
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """ Display 'hello HBNB!' when accessinn the route """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """ Display HBNB when accessimg route """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
