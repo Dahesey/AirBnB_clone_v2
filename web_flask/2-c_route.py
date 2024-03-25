b9ad153bc068ef20d0e54f6c591f42b7bec7740f
Cynthia Dodzi <dahesey@gmail.com>
2024-03-25 15:10:40 +0000

    Added 2 blank lines

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,19 +9,19 @@ from flask import escape
 
 app = Flask(__name__)
 
-""" Defining first route"""
+
 @app.route('/', strict_slashes=False)
 def hbnb():
     """ Display below text when route is accessed """
     return "Hello HBNB!"
 
-""" Define second Route """
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_route():
     """ Display below text when route is accessed """
     return "HBNB"
 
-""" Defining third route """
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """ Display C followed by text """
